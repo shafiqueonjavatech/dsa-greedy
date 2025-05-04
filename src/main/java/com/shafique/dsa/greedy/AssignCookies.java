8d1e8d545aa707063603f9c6083cea0967938e2d
mohammed-shafique-maersk <mohammed.shafique@lns.maersk.com>
2025-05-04 12:39:08 +0200

    First commit: Assign Cookies Greedy method problem.

---
+++ src/main/java/com/shafique/dsa/greedy/AssignCookies.java
@@ -0,0 +1,32 @@
+package com.shafique.dsa.greedy;
+
+import java.util.Arrays;
+
+public class AssignCookies {
+    public static void main ( String[] args ) {
+        int[] studentGreedyFactor = {1, 2, 3};
+        int[] cookySize = {1, 1};
+        System.out.println (AssignCookies.countOfSatisfiedStudent ( studentGreedyFactor
+                , cookySize ) );
+        
+    }
+    
+    private static int countOfSatisfiedStudent(int[] greedyStudent, int[] cookySize){
+        if(greedyStudent == null || greedyStudent.length == 0 || cookySize == null || cookySize.length == 0){
+            return 0;
+        }
+        
+        Arrays.sort ( greedyStudent );
+        Arrays.sort ( cookySize );
+        
+        int satisfiedStudentCount = 0;
+        int i = 0, j = 0;
+        while(i < greedyStudent.length && j < cookySize.length){
+            if(cookySize[i] >= greedyStudent[i] ){
+                satisfiedStudentCount++;
+                j++;
+            }
+        }
+       return satisfiedStudentCount;
+    }
+}
